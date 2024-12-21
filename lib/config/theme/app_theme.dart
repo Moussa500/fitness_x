24257500cf45fff75776a2ac1afefcdfa68fcb90
moussa500 <mohamed.moussa.0720@gmail.com>
2024-12-21 11:09:13 +0100

    addeed theme settings

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final lightTheme = ThemeData(
+    scaffoldBackgroundColor: Colors.white,
+    brightness: Brightness.light,
+    fontFamily: "Poppins",
+  );
+}
