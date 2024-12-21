895b68e954a602bccd13e2a5a5cf3f91b54fb238
moussa500 <mohamed.moussa.0720@gmail.com>
2024-12-21 09:27:16 +0100

    defined project colors

---
+++ lib/config/theme/app_colors.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const blueLinear =
+      LinearGradient(colors: [Color(0xff92A3FD), Color(0xff9DCEFF)]);
+  static const purpleLinear =
+      LinearGradient(colors: [Color(0xffC58BF2), Color(0xffEEA4CE)]);
+  static const caloriesLinear =
+      LinearGradient(colors: [Color(0xffC58BF2), Color(0xffB4C0FE)]);
+  static const progressBarLinear =
+      LinearGradient(colors: [Color(0xffC58BF2), Color(0xff92A3FD)]);
+  static const waterIntakeLinear =
+      LinearGradient(colors: [Color(0xffC58BF2), Color(0xffB4C0FE)]);
+  static const logoLinear =
+      LinearGradient(colors: [Color(0xffCC8FED), Color(0xff9DCEFF)]);
+  static const gray1 = Color(0xff7B6F72);
+  static const gray2 = Color(0xffADA4A5);
+  static const gray3 = Color(0xffDDDADA);
+}
